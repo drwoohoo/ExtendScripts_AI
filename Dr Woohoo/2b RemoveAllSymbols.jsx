23f732fc9232faff50d23ae78344ab5fe4716088
Dr. Woohoo! <info@drwoohoo.com>
2011-09-13 10:54:58 +0200

    Edited Dr Woohoo/2b RemoveAllSymbols.jsx via GitHub

--- Dr Woohoo/2b RemoveAllSymbols.jsx
+++ Dr Woohoo/2b RemoveAllSymbols.jsx
@@ -1,1 +1,13 @@
-﻿/****  * Removes Symbols from the Symbols Panel. * * Distributed under the MIT license. See http://www.opensource.org/licenses/mit-license.php for details. *  * Copyright (c) 2011, Dr. Woohoo! * http://DrWoohoo.com *  * All rights reserved.  ***/var doc = app.activeDocument;var proceed = confirm("Are you sure you want to delete all the Symbols?");if (proceed) {	var mySymbols = doc.symbols;	mySymbols.removeAll();}
+// Removes Symbols from the Symbols Panel.
+// Distributed under the MIT license. See http://www.opensource.org/licenses/mit-license.php for details.
+// Copyright (c) 2011, Dr. Woohoo!
+// http://DrWoohoo.com
+// All rights reserved.
+
+
+var doc = app.activeDocument;
+var proceed = confirm("Are you sure you want to delete all the Symbols?");
+if (proceed) {
+	var mySymbols = doc.symbols;
+	mySymbols.removeAll();
+}
